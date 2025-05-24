518d42511ec6973074f0e4891a70bad936af6945
Your Name <supriyoghorui645@gmail.com>
2025-05-24 12:08:23 +0530

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const express= require('express')
-const path=require('path')
-const mongoose =require('mongoose')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
-const PORT=8000
+const path = require("path");
+const methodOverride = require("method-override");
 
-const app=express()
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wanderlust", {
-}).then(() => {
-    console.log("MongoDB connected");
-}).catch((err) => {
-    console.error("MongoDB connection error:", err);
-});
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
@@ -28,8 +34,49 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", { allListings });
 });
 
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
 
 
-app.listen(PORT,()=>{
-    console.log(`Server is running at port ${PORT}`)
-})
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
