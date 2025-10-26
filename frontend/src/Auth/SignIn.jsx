87bb0205d5eab558b846f83880aac5a4b3ad61bc
Supriyo Ghorui <supriyoghorui645@gmail.com>
2025-10-27 02:31:33 +0530

    change

--- frontend/src/Auth/SignIn.jsx
+++ frontend/src/Auth/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/Authcontext';
 
 function LoginPage() {
     const [formData, setFormData] = useState({
